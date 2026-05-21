bf3f0ff4ddfaf3964aaf57f938c0d5c27acab555
Doris R <doris0309@gmail.com>
2026-05-21 19:10:37 -0400

    primer commit: prueba de inicio de aplicacion, configuracion basica

---
+++ src/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const db = require('./db'); // Importa tu conexión
+const app = express();
+
+// Configurar Handlebars
+app.set('view engine', 'hbs');
+
+// Ruta principal
+app.get('/', async (req, res) => {
+  try {
+    // 1. Consultar a la BD
+    const [rows] = await db.query('SELECT 1 + 1 AS Total;');
+    
+    // 2. Renderizar la vista enviando los datos obtenidos
+    res.render('index', { result: rows });
+  } catch (error) {
+    res.status(500).send('Error al obtener datos: ' + error.message);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
